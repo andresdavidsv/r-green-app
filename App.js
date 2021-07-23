508fe402e611533f66760bf2db119b18ccdf20c2
andresdavidsv6connex <andresdavidsolartevidal@gmail.com>
2021-07-22 20:04:19 -0500

    :sparkles: Add styles and new views.

--- App.js
+++ App.js
@@ -1,28 +1,37 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Root} from 'native-base';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+//Componentes
+import Login from './src/Views/Login';
+import SignUp from './src/Views/SignUp';
 
 const App = () => {
   return (
     <>
+      {/* <Root> */}
       <StatusBar />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{title: 'Login', headerShown: false}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: 'Sign Up'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+      {/* </Root> */}
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-});
 
 export default App;
---
+++ src/Views/Login/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View} from 'react-native';
+import {
+  Container,
+  Button,
+  Text,
+  Form,
+  H1,
+  Input,
+  Item,
+  Toast,
+} from 'native-base';
+import {useNavigation} from "@react-navigation/native";
+
+//styles
+import globalStyles from '../../styles/global';
+import styles from './styles';
+
+const Login = () => {
+  const navigation = useNavigation();
+  return (
+    <Container style={[globalStyles.container, styles.container]}>
+      <View style={globalStyles.content}>
+        <H1 style={globalStyles.title}>R GREEN</H1>
+        <Form>
+          <Item inlineLabel last style={globalStyles.input}>
+            <Input placeholder="Email" />
+          </Item>
+          <Item inlineLabel last style={globalStyles.input}>
+            <Input secureTextEntry={true} placeholder="Password" />
+          </Item>
+        </Form>
+        <Button square block style={globalStyles.button}>
+          <Text tyle={globalStyles.buttonTxt}>Login</Text>
+        </Button>
+        <Text
+          onPress={() => navigation.navigate('SignUp')}
+          style={globalStyles.hipperText}>
+          Sign Up
+        </Text>
+      </View>
+    </Container>
+  );
+};
+
+export default Login;
